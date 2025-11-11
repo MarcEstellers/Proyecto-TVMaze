f79b64397bd2d88f64413cd617b92cf3814020f1
MarcEstellers <marc.estellers@gmail.com>
2025-11-11 18:53:43 +0100

    Creando el buscador

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,28 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+window.addEventListener('load',function(){
+  const btn = this.document.getElementById('btnBuscar');
+  btn.addEventListener('click', Buscar);
+  function Buscar(){
+    const serie = document.getElementById('BuscadorSeries');
+    const busqueda = document.getElementById('Historial_de_buscados');
+    busqueda.innerHTML = serie.value;
+    localStorage.setItem('serieBuscada',serie.value);
+    return true;
+  }
+})
+
   return (
-    <h1>Segundo Comit</h1>
+    <>
+      <h1>Buscador de Series</h1>
+      <form id="form1">
+        <label id="label1">Busca Serie: </label>
+        <input type="text" id="BuscadorSeries" placeholder="busca serie" name="serieBuscada"/>
+      </form>
+      <button id="btnBuscar">Buscar</button>
+      <p id="Historial_de_buscados"></p>
+    </>
   )
 }
 
