a62783745885952a9fb12d9d46076ea4181d41ec
MarcEstellers <marc.estellers@gmail.com>
2025-11-11 20:04:44 +0100

    NavBary Css

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './componentes/NavBar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-window.addEventListener('load',function(){
-  const btn = this.document.getElementById('btnBuscar');
-  btn.addEventListener('click', Buscar);
-  function Buscar(){
-    const serie = document.getElementById('BuscadorSeries');
-    const busqueda = document.getElementById('Historial_de_buscados');
-    busqueda.innerHTML = serie.value;
-    localStorage.setItem('serieBuscada',serie.value);
-    return true;
-  }
-})
 
+  
   return (
     <>
-      <h1>Buscador de Series</h1>
-      <form id="form1">
-        <label id="label1">Busca Serie: </label>
-        <input type="text" id="BuscadorSeries" placeholder="busca serie" name="serieBuscada"/>
-      </form>
-      <button id="btnBuscar">Buscar</button>
-      <p id="Historial_de_buscados"></p>
+      <NavBar/>
     </>
   )
 }
---
+++ src/data/data.jsx
@@ -0,0 +1,27 @@
+export const NavBarlinks = [
+
+    {
+        id: 1,
+        title: 'HOME',
+        link: '/'
+    },
+
+    {
+        id: 2,
+        title: 'FAVORITOS',
+        link: '/'
+    },
+
+    {
+        id: 3,
+        title: 'INFORMACION',
+        link: '/'
+    },
+
+    {
+        id: 4,
+        title: 'CONTACTOS',
+        link: '/'
+    },
+
+]
